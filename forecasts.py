80659345e7e0f4bedddf5883f34a0fef7f27c96c
vaishnavikanna <kannavaishnavi@uni-koblenz.de>
2023-05-11 18:03:47 +0200

    combining dragino and deltaohm coordinates, added scheduler code, removed some metadata

--- forecasts.py
+++ forecasts.py
@@ -8,6 +8,7 @@ import openpyxl
 from dotenv.main import load_dotenv
 import pyarrow.parquet as pq
 from fastparquet import write, ParquetFile, update_file_custom_metadata
+from datetime import datetime
 import pyarrow as pa
 import pyarrow.json as pj
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -19,28 +20,20 @@ load_dotenv()
 
 # Reading the excel file using pandas to extract coordinates of dragino and deltaohm
 filename = 'InventarStatischeSensoren.xlsx'
-dragino = pd.read_excel(filename, sheet_name='Dragino LWS')
-deltaohm = pd.read_excel(filename, sheet_name='DeltaOhm LWS')
+df_coordinates = pd.read_excel(filename, sheet_name='All_Coordinates')
 
 
 # Function to extract and preprocess required data from the excel file
 def extract_data(df):
-    coordinates = df['Coordinates'].tolist()
-    sensor_id = df['ID_TTS'].tolist()
-    #eui = df['EUI'].tolist()
-    laubwand = df['Laubwand'].tolist()
-    ausbringungsort = df['Ausbringungsort'].tolist()
     df[['Latitude', 'Longitude']] = df['Coordinates'].apply(lambda x: pd.Series(str(x).split(",")))
     df_data = df[['Latitude', 'Longitude']]
-    df_meta = df_data.assign(ID_TTS=sensor_id, Laubwand=laubwand, Ausbringungsort=ausbringungsort)
-    df_preprocessed = df_meta.dropna()
+    df_preprocessed = df_data.dropna()
     df_preprocessed.reset_index(drop=True, inplace=True)
     return df_preprocessed
 
 
 # passing the data from excel file to extract_data function
-df_dragino = extract_data(dragino)
-df_deltaohm = extract_data(deltaohm)
+df_response = extract_data(df_coordinates)
 
 
 # Function to loop over the coordinates of Dragino and Deltaohm and making meteoblue api calls to receive forecast data
@@ -52,7 +45,7 @@ def forecasts(df):
         secret_key=os.environ['MINIO_SECRET_KEY'],
         secure=False
     )
-
+    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     # checking if project bucket exists
     project_bucket = client.bucket_exists("projekt-daten")
     if not project_bucket:
@@ -64,24 +57,16 @@ def forecasts(df):
     for i in range(len(df)):
         latitude = df.loc[i, 'Latitude']
         longitude = df.loc[i, 'Longitude']
-        sensor_id = df.loc[i, 'ID_TTS']
-        #eui = df.loc[i, 'EUI']
-        laubwand = df.loc[i, 'Laubwand']
-        ausbringungsort = df.loc[i, 'Ausbringungsort']
         api_url = "https://my.meteoblue.com/packages/agro-1h_agromodelleafwetness-1h?apikey=946f8e5caba1&lat=" + latitude + "&lon=" + longitude + "&asl=75&format=json&tz=Europe%2FBerlin"
         response = requests.get(api_url).json()
-        response['metadata']['name'] = sensor_id
-        #response['metadata']['EUI'] = eui
-        response['metadata']['Laubwand'] = laubwand
-        response['metadata']['Ausbringungsort'] = ausbringungsort
         jsonresponse = json.dumps(response)
 
         # creating a new json file for each sensor for each run
-        with open(sensor_id + ".json", 'w') as f:
+        with open(str(latitude) + "_" + str(longitude) + "_" + current_time + ".json", 'w') as f:
             f.write(jsonresponse)
 
         # opening the json file created earlier to create a parquet file
-        with open(sensor_id + ".json", 'r') as f:
+        with open(str(latitude) + "_" + str(longitude) + "_" + current_time + ".json", 'r') as f:
             data = json.loads(f.read())
 
         # normalizing the json to extract only relevant data records using the key data_1h
@@ -100,28 +85,26 @@ def forecasts(df):
         # writing data and metadata to parquet format
         # creating a new parquet file if the file does not exist already
         # if the parquet file exists already then data is appended to the existing file with fastparquet
-        if not os.path.isfile(sensor_id + ".parquet"):
-            write(sensor_id + ".parquet", df_data, custom_metadata=custom_metadata)
+        if not os.path.isfile(str(latitude) + "_" + str(longitude) + ".parquet"):
+            write(str(latitude) + "_" + str(longitude) + ".parquet", df_data, custom_metadata=custom_metadata)
         else:
-            write(sensor_id + ".parquet", df_data, custom_metadata=custom_metadata, append=True)
+            write(str(latitude) + "_" + str(longitude) + ".parquet", df_data, custom_metadata=custom_metadata, append=True)
 
         # adding json files to minio bucket
         if project_bucket:
-            client.fput_object("projekt-daten",
-                               "UniKo/Egov/WetterDaten/Meteoblue/json-formatted/" + os.path.basename(f.name), f.name)
+            client.fput_object("projekt-daten", "UniKo/Egov/WetterDaten/Meteoblue/json-formatted/" + os.path.basename(f.name), f.name)
 
     # adding parquet files to minio bucket
     extension = "*.parquet"
     parquet_files_list = [f for f in glob.glob(extension)]
     for parquet_file_name in parquet_files_list:
         if project_bucket:
-            client.fput_object("projekt-daten",
-                               "UniKo/Egov/WetterDaten/Meteoblue/parquet-formatted/" + parquet_file_name, parquet_file_name)
+            client.fput_object("projekt-daten", "UniKo/Egov/WetterDaten/Meteoblue/parquet-formatted/" + parquet_file_name, parquet_file_name)
 
     print("Successful responses uploaded to Minio")
 
 
-'''
+
 #adding forecasts function to the scheduler that makes api calls each hour
 if __name__ == '__main__':
 
@@ -129,10 +112,8 @@ if __name__ == '__main__':
     trigger = CronTrigger(
         year="*", month="*", day="*", hour="*", minute="0", second="0"
     )
-    scheduler.add_job(forecasts(), trigger=trigger)
+    scheduler.add_job(forecasts, args=[df_response], trigger=trigger)
     scheduler.start()
-
-    #print("Minio Objects" + str(round(objects_created._value.get(), 9)))
 
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
@@ -142,6 +123,5 @@ if __name__ == '__main__':
     except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
 
-'''
-forecasts(df_dragino)
-forecasts(df_deltaohm)
+
+#forecasts(df_response)
